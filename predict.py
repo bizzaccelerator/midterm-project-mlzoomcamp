d4e4816f40895e075b69d1aed6d11e91c6196f9e
Jobert Gutierrez Gonzalez <jobert.gutierrez@gmail.com>
2024-11-22 03:22:05 -0500

    Predict service exposed through flask.

--- predict.py
+++ predict.py
@@ -1,24 +1,27 @@
 import pickle
+from flask import Flask
+from flask import request, jsonify
 
 # The model used is refered
 model_file = 'model_Grid_GBT_learnig=0.1_depth=3.bin'
-
-# A farmer of test is defined as:
-farmer = {'education': 'Certificate',
-        'age_bracket': '36-45',
-        'household_size': '7',
-        'laborers': '2',
-        'main_advisory_source': 'Radio',
-        'acreage': '2.0',
-        'fertilizer_amount': 50}
 
 # Extracting the vectorizer and the model:
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-# Calculating the target variable as:
-X = dv.transform([farmer])
-y_pred = model.predict(X)[0]
+# Instanciating the app
+app = Flask('yield')
 
-print('Input: ', farmer)
-print('Yield prediction: ', y_pred)
+@app.route('/predict', methods=['POST'])
+# Function that calculates the target variable:
+def predict():
+    farmer = request.get_json()
+    X = dv.transform([farmer])
+    y_pred = model.predict(X)[0]
+    result = {
+        'Yield prediction': y_pred,
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
